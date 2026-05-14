fa2bb129da575b5d8e4d9dbede02ab0ba04b2651
Глухов Дмитрий Олегович <dzmitry.hlukhau@softclub.by>
2026-05-14 10:47:50 +0300

    Remove obsolete deployment scripts for DPA, DPR, PHA, PPV, and rights service. These scripts were previously used for building and deploying respective applications but are no longer needed.

--- build-and-deploy-rights-service-local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Сборка eec-rights-service (WAR) и копия в $TOMCAT_HOME/webapps (см. deploy-rights-service-local.sh).
-# Использование: ./build-and-deploy-rights-service-local.sh
-
-set -e
-PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$PROJECT_DIR"
-./build-rights-service.sh
-./deploy-rights-service-local.sh
--- build-and-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Обратная совместимость: вызывает сборку и развёртывание DPA.
-# Использование: ./build-and-deploy.sh  (то же, что ./build-and-deploy-dpa.sh)
-
-set -e
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-exec "$SCRIPT_DIR/build-and-deploy-dpa.sh"
